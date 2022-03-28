8e694676aeb18ab3818dad6ba2e63464422ab150
deekshant57 <deekshant57@gmail.com>
2022-03-28 11:08:57 +0530

    Update user.model.js

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -22,6 +22,16 @@ const userSchema = new mongoose.Schema(
   }
 );
 
+userSchema.pre("save", (next) => {
+  const hash = bcrypt.hashSync(this.password, 8);
+  this.password = hash;
+  return next();
+});
+
+userSchema.methods.checkPassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
 const User = mongoose.model("user", userSchema);
 
 module.exports = User;
